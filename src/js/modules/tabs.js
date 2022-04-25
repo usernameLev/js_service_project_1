dab074221d301f54af61c85768bae147aaa6b522
Lev <tyanlev1989@gmail.com>
2022-04-25 20:07:25 +0500

    004 fixed 2

--- src/js/modules/tabs.js
+++ src/js/modules/tabs.js
@@ -4,37 +4,37 @@ export const tabs = ({
   contentSelector,
   activeClass,
 }) => {
-  const headers = document.querySelector(headerSelector),
-    tab = document.querySelectorAll(tabSelector),
-    content = document.querySelectorAll(contentSelector);
+  const header = document.querySelector(headerSelector),
+    tabs = document.querySelectorAll(tabSelector),
+    contents = document.querySelectorAll(contentSelector);
 
   const hideTabContents = () => {
-    content.forEach((hideTabContent) => {
-      hideTabContent.style.display = 'none';
+    contents.forEach((content) => {
+      content.style.display = 'none';
     });
 
-    tab.forEach((hideTabContent) => {
-      hideTabContent.classList.remove(activeClass);
+    tabs.forEach((tab) => {
+      tab.classList.remove(activeClass);
     });
   };
 
   const showTabContent = (i = 0) => {
-    content[i].style.display = 'block';
-    tab[i].classList.add(activeClass);
+    contents[i].style.display = 'block';
+    tabs[i].classList.add(activeClass);
   };
 
   hideTabContents();
   showTabContent();
 
-  headers.addEventListener('click', (e) => {
+  header.addEventListener('click', (e) => {
     const target = e.target;
     if (
       target &&
       (target.classList.contains(tabSelector.replace(/\./, '')) ||
         target.parentNode.classList.contains(tabSelector.replace(/\./, '')))
     ) {
-      tab.forEach((hideTabContent, i) => {
-        if (target == hideTabContent || target.parentNode == hideTabContent) {
+      tabs.forEach((tab, i) => {
+        if (target == tab || target.parentNode == tab) {
           hideTabContents();
           showTabContent(i);
         }
